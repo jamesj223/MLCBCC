d242efea59037f4ea74fa8333d25304f06c53550
jamesj223 <jamesj223@gmail.com>
2023-02-23 16:00:19 +1100

    Added classifier/training file and updated that section of readme accordingly

---
+++ MLE_2_Training_Attempt_1.py
@@ -0,0 +1,186 @@
+#!python2
+###############################################################################
+# MachineLearningExperiment.py - Machine Learning Comic Book Cover Finding Experiment
+# jamesj223
+
+# I think recall is the most important stat for this project?
+
+# Accuracy
+# TODO
+
+# Log Loss
+# TODO
+
+# Precision - https://scikit-learn.org/stable/modules/generated/sklearn.metrics.precision_score.html
+# The precision is the ratio tp / (tp + fp) where tp is the number of true positives and fp the number of false positives. 
+# The precision is intuitively the ability of the classifier not to label as positive a sample that is negative.
+# The best value is 1 and the worst value is 0.
+
+# Recall - https://scikit-learn.org/stable/modules/generated/sklearn.metrics.recall_score.html
+# The recall is the ratio tp / (tp + fn) where tp is the number of true positives and fn the number of false negatives. 
+# The recall is intuitively the ability of the classifier to find all the positive samples.
+# The best value is 1 and the worst value is 0.
+
+# F1 Score - https://scikit-learn.org/stable/modules/generated/sklearn.metrics.f1_score.html
+# The F1 score can be interpreted as a weighted average of the precision and recall.
+# The relative contribution of precision and recall to the F1 score are equal. 
+# The formula for the F1 score is: F1 = 2 * (precision * recall) / (precision + recall)
+# The best value is 1 and the worst value is 0.
+
+# Raw Stats?
+# Correct
+# False Positives
+# False Negatives
+
+###############################################################################
+# Initialisation
+
+from datetime import datetime
+
+import cPickle
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score, log_loss, precision_score, recall_score, f1_score, confusion_matrix
+
+# ML Algorithms
+from sklearn.dummy import DummyClassifier # Lol
+from sklearn.ensemble import RandomForestClassifier
+
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC, LinearSVC, NuSVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, GradientBoostingClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+
+#from tqdm import tqdm
+
+def warn(*args, **kwargs): pass
+import warnings
+warnings.warn = warn
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+###############################################################################
+# Config
+
+###############################################################################
+# Classes
+
+###############################################################################
+# Functions
+
+def save_obj(obj, name, protocol=2):
+  with open('obj/'+ name + '.pkl', 'wb') as f:
+      cPickle.dump(obj, f, protocol)
+
+###############################################################################
+# Main
+
+if __name__ == '__main__':   
+
+    startTime = datetime.now()
+
+    print("Start - " + str(datetime.now()))
+    print("")
+
+    ### Code Goes Here
+
+    # Pandas/Train Stuff
+
+    train = pd.read_csv('TrainingSet2.csv')
+
+    y=train['label'].values
+
+    x = train.drop('label', 1)
+
+    # :( Need to figure out how to do this better
+    # File names are kinda important for what I want...
+    x = x.drop('fileName', 1) 
+
+    x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=0, test_size=0.2)
+
+    # SK Learn Pipe Stuff
+
+    # pipe = make_pipeline(
+    #   StandardScaler(),
+    #   #RandomForestClassifier(random_state=0)
+    #   DummyClassifier(strategy="uniform")
+    # )
+
+    # pipe.fit(x_train, y_train)
+
+    # accuracyScore = accuracy_score(pipe.predict(x_test), y_test)
+
+    #print("Accuracy: {:.4%}".format(accuracyScore))
+
+    # Multi Classifier Showdown
+    classifiers = [
+        KNeighborsClassifier(3),
+        #SVC(kernel="linear", C=0.1, probability=True), 
+        #NuSVC(probability=True),
+        DecisionTreeClassifier(),
+        RandomForestClassifier(),
+        AdaBoostClassifier(),
+        GradientBoostingClassifier(),
+        GaussianNB(),
+        LinearDiscriminantAnalysis(),
+        #QuadraticDiscriminantAnalysis()
+    ]
+
+    for clf in classifiers:
+
+        name = clf.__class__.__name__
+
+        #pipe = make_pipeline(
+        #   StandardScaler(),
+        #   clf
+        #)
+
+        #pipe.fit(x_train, y_train)
+        clf.fit(x_train, y_train)
+
+        save_obj(clf, name)
+
+        train_predictions = clf.predict(x_test)
+        train_predictions_proba = clf.predict_proba(x_test)
+
+        print("="*30)
+        print(name)
+        #print('****Results****')
+
+        acc = accuracy_score(y_test, train_predictions)
+        print("Accuracy: {:.4%}".format(acc))
+
+        precision = precision_score(y_test, train_predictions)
+        print("Precision: {}".format(precision))
+
+        recall = recall_score(y_test, train_predictions)
+        print("Recall: {}".format(recall))
+
+        f1 = f1_score(y_test, train_predictions)
+        print("F1 Score: {}".format(f1))
+
+        logloss = log_loss(y_test, train_predictions_proba)
+        print("Log Loss: {}".format(logloss))
+
+        print("")
+        C = confusion_matrix(y_test, train_predictions)
+        print("True Positives: {}".format(C[1,1]))
+        print("True Neagtives: {}".format(C[0,0]))
+        print("False Positives: {}".format(C[0,1]))
+        print("False Negatives: {}".format(C[1,0]))
+
+
+
+        print("")
+
+
+    ### Code Ends Here
+
+    print("End - " + str(datetime.now()))
+
+    print("Took: " + str( datetime.now() - startTime ))
--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 A series of scripts to use machine learning to find and extract covers from comic books. 
 
-Most comic book files will have the cover as the first page, but often they will have multiple covers. Sometimes these are included at the start, sometimes at the end, and sometimes they are spread throughout the book. The goal of this project is to be able to run it against a directory full of comic book files, and have it extract all of the images it thinks are covers, so that they can be used to generate a cool collage (see the examples section below)
+Most comic book files will have the cover as the first page, but often they will have multiple covers. Sometimes these are included at the start, sometimes at the end, and sometimes they are spread throughout the book. The goal of this project is to be able to run it against a directory full of comic book files, and have it extract all of the covers, so that they can be used to generate a cool collage (see the examples section below)
 
 ## Examples
 
@@ -38,6 +38,27 @@ The features we are using are as follows:
 
 ## Training
 
+MLE_2_Training_Attempt_1.py is the second main file. Given a training data set, split it 80:20 training:test, then run various different classifiers using those two sets and measure their performance.
+
+Key metrics we are measureing are (Accuracy)[https://scikit-learn.org/stable/modules/generated/sklearn.metrics.accuracy_score.html#sklearn.metrics.accuracy_score], (Precision)[https://scikit-learn.org/stable/modules/generated/sklearn.metrics.precision_score.html], (Recall)[https://scikit-learn.org/stable/modules/generated/sklearn.metrics.recall_score.html], (F1)[https://scikit-learn.org/stable/modules/generated/sklearn.metrics.f1_score.html] and (Logistic Loss)[https://scikit-learn.org/stable/modules/generated/sklearn.metrics.log_loss.html#sklearn.metrics.log_loss].
+
+The classifiers tested are:
+
+- [RandomForestClassifier](https://scikit-learn.org/stable/modules/generated/sklearn.ensemble.RandomForestClassifier.html#sklearn.ensemble.RandomForestClassifier)
+- [GradientBoostingClassifier](https://scikit-learn.org/stable/modules/generated/sklearn.ensemble.GradientBoostingClassifier.html)
+- [LinearDiscriminantAnalysis](https://scikit-learn.org/stable/modules/generated/sklearn.discriminant_analysis.LinearDiscriminantAnalysis.html)
+- [AdaBoostClassifier](https://scikit-learn.org/stable/modules/generated/sklearn.ensemble.AdaBoostClassifier.html#sklearn.ensemble.AdaBoostClassifier)
+- [SVC](https://scikit-learn.org/stable/modules/generated/sklearn.svm.SVC.html#sklearn.svm.SVC)
+- [GaussianNB](https://scikit-learn.org/stable/modules/generated/sklearn.naive_bayes.GaussianNB.html#sklearn.naive_-bayes.GaussianNB)
+- [DecisionTreeClassifier](https://scikit-learn.org/stable/modules/generated/sklearn.tree.DecisionTreeClassifier.html#sklearn.tree.DecisionTreeClassifier)
+- [KNeighborsClassifier](https://scikit-learn.org/stable/modules/generated/sklearn.neighbors.KNeighborsClassifier.html#sklearn.neighbors.KNeighborsClassifier)
+
+The results of the tests looked like this:
+
+![Comparison3](/Images/ClassifierComparison3.png)
+
+Overall, GradientBoostingClassifier was found to be the best option for this use case.
+
 ## Testing
 
 ## Early Experiments/Testing
@@ -49,4 +70,7 @@ Comparison of different classifiers and how they performed against various train
 ![Comparison1](/Images/ClassifierComparison1.png)
 ![Comparison2](/Images/ClassifierComparison2.png)
 ![Comparison3](/Images/ClassifierComparison3.png)
+
+Examining computation/time cost of different feature types
+
 ![FeatureCost1](/Images/FeatureComputationCost.png)
